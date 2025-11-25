40dea9ab2362882c89ee955865438cb333985afb
russellreneroring-tech <russellreneroring@gmail.com>
2025-11-25 14:13:43 +0800

    Create Program Kuis PG.py

---
+++ Program Kuis PG.py
@@ -0,0 +1,8 @@
+# ==============================
+# Program Kuis Pilihan Ganda
+# ==============================
+
+print("===== Selamat Datang di Kuis Python! =====")
+print("Jawab pertanyaan dengan mengetik A, B, C, atau D\n")
+
+score = 0
