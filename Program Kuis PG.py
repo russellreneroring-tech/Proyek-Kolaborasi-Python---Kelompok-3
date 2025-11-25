75f8caa084057d3b2bf9cbf0dce5256413f934a6
russellreneroring-tech <russellreneroring@gmail.com>
2025-11-25 14:29:51 +0800

    Delete Program Kuis PG.py

--- Program Kuis PG.py
+++
@@ -1,8 +0,0 @@
-# ==============================
-# Program Kuis Pilihan Ganda
-# ==============================
-
-print("===== Selamat Datang di Kuis Python! =====")
-print("Jawab pertanyaan dengan mengetik A, B, C, atau D\n")
-
-score = 0
