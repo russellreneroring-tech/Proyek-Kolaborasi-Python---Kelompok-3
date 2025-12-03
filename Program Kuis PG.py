62a4d2c324d44c0fa5dada5ca1f92b3b02248fd7
russellreneroring-tech <russellreneroring@gmail.com>
2025-12-03 13:41:29 +0800

    Update Program Kuis PG.py

--- Program Kuis PG.py
+++ Program Kuis PG.py
@@ -8,8 +8,8 @@ print("Jawab pertanyaan dengan mengetik A, B, C, atau D\n")
 score = 0
 
 
-# Daftar pertanyaan (dictionary)uestions = [
-    {
+# Daftar pertanyaan (dictionary)
+questions = [{
         "question": "1. Apa singkatan dari CPU?",
         "options": ["A. Central Processing Unit", 
                     "B. Control Primary Unit", 
@@ -32,5 +32,28 @@ score = 0
                     "C. C++", 
                     "D. Java"],
         "answer": "A"
-    }
+    } 
 ]
+for q in questions:
+    print(q["question"])
+    for opt in q["options"]:
+        print(opt)
+
+    user_answer = input("Jawaban kamu: ").strip().upper()
+
+    if user_answer == q["answer"]:
+        print("Benar!\n")
+        score += 1
+    else:
+        print(f"Salah! Jawaban yang benar adalah {q['answer']}\n")
+
+
+print("===== HASIL AKHIR =====")
+print(f"Skor kamu: {score} dari {len(questions)}")
+
+if score == len(questions):
+    print("Keren! Kamu menjawab semua dengan benar!")
+elif score >= 2:
+    print("Bagus! Kamu sudah cukup paham.")
+else:
+print("Jangan menyerah, terus belajar ya!")
